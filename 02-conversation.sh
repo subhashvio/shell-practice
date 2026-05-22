5505f89cf13666993cb1dd72f05068d3a51aa9f1
Subhash Bairagoni <subhash.vio@gmail.com>
2026-05-22 20:19:33 +0530

    hello world

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -5,3 +5,11 @@ echo "Biden: I'm doing well, thank you. How about you?"
 echo "Trump: I'm doing great, as always. I just wanted to talk to you about the economy."
 echo "Biden: Sure, what do you want to discuss?"
 echo "Trump: I think the economy is doing fantastic under my administration. The stock market is booming and unemployment is at an all-time low."
+echo "Biden: I disagree. The economy
+echo "
+echo "kjhkh"
+echo"ljsdf"
+
+
+echo "dlfkjldskfjf"
+echo 
