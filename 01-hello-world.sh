d8768a6f6efd9352ec8f39da8e71816060f55a7c
Subhash Bairagoni <subhash.vio@gmail.com>
2026-05-22 19:58:27 +0530

    hello world

---
+++ 01-hello-world.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# this is a comment
+
+echo "Hello, World"
+echo "This is a bash script"
+echo "Bash is a powerful scripting language"
+echo "You can use it to automate tasks"
+echo "Bash scripts can be run on any Unix-like operating system"
