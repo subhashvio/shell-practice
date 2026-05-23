9353af8857d06e1c896bc549f46f9051f666a372
Subhash Bairagoni <subhash.vio@gmail.com>
2026-05-23 07:25:52 +0530

    hello world

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -3,7 +3,7 @@
 PERSON1=Trump
 PERSON2=Biden
 
-echo "$PERSON1: Hello, Biden. How are you doing today?"
+echo "$PERSON1: Hello, $PERSON2. How are you doing today?"
 echo "$PERSON2: I'm doing well, thank you. How about you?"
 echo "$PERSON1: I'm doing great, as always. I just wanted economy."
 echo "$PERSON2: Sure, what do you want to discuss?"
