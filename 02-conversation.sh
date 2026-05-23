435bc5284721e14ee410a2f6ef50a5fbf9448961
Subhash Bairagoni <subhash.vio@gmail.com>
2026-05-23 07:20:36 +0530

    hello world

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -5,8 +5,8 @@ PERSON2=Biden
 
 echo "$PERSON1: Hello, Biden. How are you doing today?"
 echo "$PERSON2: I'm doing well, thank you. How about you?"
-echo "T$PERSON1rump: I'm doing great, as always. I just wanted to talk to you about the economy."
+echo "$PERSON1: I'm doing great, as always. I just wanted economy."
 echo "$PERSON2: Sure, what do you want to discuss?"
-echo "$PERSON1: I think the economy is doing fantastic under my administration. The stock market is booming and unemployment is at an all-time low."
-echo "$PERSON2: I disagree.
+echo "$PERSON1: I think the economy is doing fantastic"
+echo "$PERSON2: I disagree."
 
