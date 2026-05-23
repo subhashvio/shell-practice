7526f0a78a68cc88783640323f99261bb45fce2f
Subhash Bairagoni <subhash.vio@gmail.com>
2026-05-23 07:35:34 +0530

    hello world

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-PERSON1=Trump
-PERSON2=Biden
+PERSON1="Trump"
+PERSON2="Biden"
 
-echo "$PERSON1: Hello, $PERSON2. How are you doing today?"
-echo "$PERSON2: I'm doing well, thank you. How about you?"
-echo "$PERSON1: I'm doing great, as always. I just wanted economy."
-echo "$PERSON2: Sure, what do you want to discuss?"
-echo "$PERSON1: I think the economy is doing fantastic"
+echo "$PERSON1: Hello, $PERSON2"
+echo "$PERSON2: I'm doing well."
+echo "$PERSON1: I'm doing great."
+echo "$PERSON2: Sure, what do you want?"
+echo "$PERSON1: I think the economy is improving."
 echo "$PERSON2: I disagree."
-
