5f646b1bd30cfe7b22ccaff5223d8d74ed002321
Subhash Bairagoni <subhash.vio@gmail.com>
2026-05-23 07:12:59 +0530

    hello world

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
-echo "Trump: Hello, Biden. How are you doing today?"
-echo "Biden: I'm doing well, thank you. How about you?"
-echo "Trump: I'm doing great, as always. I just wanted to talk to you about the economy."
-echo "Biden: Sure, what do you want to discuss?"
-echo "Trump: I think the economy is doing fantastic under my administration. The stock market is booming and unemployment is at an all-time low."
-echo "Biden: I disagree. The economy
-echo "
-echo "kjhkh"
-echo"ljsdf"
+PERSON1="Trump"
+PERSON2="Biden"
 
+echo "$PERSON1: Hello, Biden. How are you doing today?"
+echo "$PERSON2: I'm doing well, thank you. How about you?"
+echo "T$PERSON1rump: I'm doing great, as always. I just wanted to talk to you about the economy."
+echo "$PERSON2: Sure, what do you want to discuss?"
+echo "$PERSON1: I think the economy is doing fantastic under my administration. The stock market is booming and unemployment is at an all-time low."
+echo "$PERSON2: I disagree.
 
-echo "dlfkjldskfjf"
-echo 
