1bec0d962948a7addce7aa699a8c14581d84ac12
Subhash Bairagoni <subhash.vio@gmail.com>
2026-05-23 10:31:46 +0530

    hello world

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -3,12 +3,12 @@
 PERSON1=Trump
 PERSON2=Biden
 
-#echo "$PERSON1: Hello, $PERSON2"
-#echo "$PERSON2: I'm doing well."
-#echo "$PERSON1: I'm doing great."
-#echo "$PERSON2: Sure, what do you want?"
-#echo "$PERSON1: I think the economy is improving."
-#echo "$PERSON2: I d
+echo "$PERSON1: Hello, $PERSON2"
+cho "$PERSON2: I'm doing well."
+echo "$PERSON1: I'm doing great."
+echo "$PERSON2: Sure, what do you want?"
+echo "$PERSON1: I think the economy is improving."
+echo "$PERSON2: I d
 
 
 
