0a346715dce7f323b7bd41bdeb278b0892981aa8
Subhash Bairagoni <subhash.vio@gmail.com>
2026-05-24 17:43:43 +0530

    hello world script created

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -12,7 +12,7 @@ NUMBER=$1
 
 if [ $NUMBER -gt 20 ]; then
     echo "Given number $NUMBER is greater than 20"
-elseif [ $NUMBER -eq 20 ]; than
+elif [ $NUMBER -eq 20 ]; than
     echo "Given number $NUMBER is equal to 20"
 else
     echo "Given number $NUMBER is less than 20"
