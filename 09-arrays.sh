4bc004bb4f161bcbf2a092d96b704aa235bbf5a1
Subhash Bairagoni <subhash.vio@gmail.com>
2026-05-24 17:13:07 +0530

    hello world script created

--- 09-arrays.sh
+++ 09-arrays.sh
@@ -4,6 +4,6 @@
 
 MOVIES=("RRR" "Pushpa" "Varanasi")
 echo "All Movies are: ${MOVIES[@]}"
-echo "First Movie is: ${MOVIES[0]]}"
+echo "First Movie is: ${MOVIES[0]}"
 echo "Second Movie is: ${MOVIES[1]}"
 echo "Third Movie is: ${MOVIES[2]}"
