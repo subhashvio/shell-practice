8515d271ef8f6002e3e9b0a7ba14c74041c185b5
Subhash Bairagoni <subhash.vio@gmail.com>
2026-05-26 19:07:53 +0530

    hello world script created

---
+++ 15-error-handling.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+USERID=$(id -u)
+LOGS_DIR="/var/log/shell-script/"
+LOGS_FILE="$LOGS_DIR/$0.log"  #/home/ec2-user/shell-logs/12-functions-and-logs.sh
+TIMESTAMP=$(date +"%Y-%m-%d %H:%M:%S")
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+trap 'echo "Error at $LINENO", command: $BASH_COMMAND"'
+# Check if the root access or not
+
+if [ $USERID -ne 0 ]; then
+    echo "Please run the script with root access"
+    exit 1
+
+fi
+
+for package in $@
+
+    do
+        echo "$TIMESTAMP [INFO] Installing $package"
+        dnf list installed $package &>> LOGS_FILE
+
+        if [ $? -ne 0 ]; then
+            dnf install $package -y &>> LOGS_FILE
+            VALIDATE "Installing $package" $?
+        else
+            echo -e "$TIMESTAMP [INFO] package already installed ....$Y SKIPPING $N" | tee -a $LOGS_FILE
+        fi
+    done
